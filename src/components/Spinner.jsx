a59805a825fc6bc068c42a35d77db57ad9f477fa
willytucuman <tomasgroso09@hotmail.com>
2023-06-19 12:31:41 -0300

    Primera version del proyecto

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,15 @@
+import "../styles/spinner.css"
+const Spinner = () => {
+  return (
+    <div className="sk-chase">
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+  </div>
+  )
+}
+
+export default Spinner
